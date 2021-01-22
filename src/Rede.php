16955e0842d705da786b58551e52d69fac09a665
Lucas Giovanny <lucasgiovanny@gmail.com>
2021-01-22 14:52:04 +0000

    Add new methods from eRede API

--- src/Rede.php
+++ src/Rede.php
@@ -10,7 +10,9 @@ use Rede\Transaction;
 class Rede
 {
     /**
-     * Store $store
+     * eRede Store
+     * 
+     * @var Store $store
      */
     private $_store;
 
@@ -32,7 +34,7 @@ class Rede
     }
 
     /**
-     * Authorize one transaction with eRede API.
+     * Authorize one transaction. Capture by default.
      * 
      * @param float  $total      Total of the transaction
      * @param string $reference  Reference of transaction
@@ -43,7 +45,9 @@ class Rede
     public function authorize(
         float $total,
         string $reference,
-        array $creditCard
+        array $creditCard,
+        bool $capture = true,
+        int $installments = 1
     ) {
         $transaction = (new Transaction($total, $reference))
             ->creditCard(
@@ -52,10 +56,62 @@ class Rede
                 $creditCard['cardExpirationMonth'],
                 $creditCard['cardExpirationYear'],
                 $creditCard['cardHolder'],
-            );
+            )
+            ->capture($capture)
+            ->setInstallments($installments);
 
         $transaction = (new eRede($this->_store))->create($transaction);
 
         return $transaction;
+    }
+
+    /**
+     * Cancel on transaction by transaction ID.
+     *
+     * @param string $transaction
+     * 
+     * @return eRede
+     */
+    public function cancel($transaction)
+    {
+        $transaction = (new Transaction())->setTid($transaction);
+
+        return (new eRede($this->_store))->cancel($transaction);
+    }
+
+    /**
+     * Find transaction by reference
+     *
+     * @param string $reference
+     * 
+     * @return eRede
+     */
+    public function getByReference($reference)
+    {
+        return (new eRede($this->_store))->getByReference($reference);
+    }
+
+    /**
+     * Find transaction by transaction id
+     *
+     * @param string $transaction
+     * 
+     * @return eRede
+     */
+    public function get($transaction)
+    {
+        return (new eRede($this->_store))->get($transaction);
+    }
+
+    /**
+     * Find transactions refunds
+     *
+     * @param string $transaction
+     * 
+     * @return eRede
+     */
+    public function getRefunds($transaction)
+    {
+        return (new eRede($this->_store))->getRefunds($transaction);
     }
 }
